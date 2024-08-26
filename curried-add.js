ff54e333db99498e0d5489f62191906f6710ba6f
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-08-26 16:35:58 -0700

    curried-add passes

--- curried-add.js
+++ curried-add.js
@@ -1,5 +1,19 @@
-function curriedAdd(total) {
-
+function curriedAdd(addend = null) {
+    let total = 0
+    
+    function curryingFunct(addend = null) {
+        if (addend !== null) {
+            total = total + addend;
+            return curryingFunct
+        }
+        return total;
+    }
+    
+    if (addend !== null) {
+        total = total + addend;
+        return curryingFunct;
+    }
+    return total;
 }
 
 module.exports = { curriedAdd };
