463e8d90c74e6e99560d6f0da9fe7d12213dc499
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-08-26 16:24:47 -0700

    guessing game done

--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,23 @@
 function guessingGame() {
-
+    const secret = Math.floor(Math.random()*100);
+    let gameWon = false;
+    let guesses = 0;
+    
+    return function(guess) {
+        if (gameWon) {
+            return "The game is over, you already won!"
+        }
+        
+        guesses++;
+        if (guess === secret) {
+            gameWon = true;
+            return `You win! You found ${secret} in ${guesses} guesses.`
+        }
+        if (guess < secret) {
+            return `${guess} is too low!`
+        }
+        return `${guess} is too high!`
+    }
 }
 
 module.exports = { guessingGame };
