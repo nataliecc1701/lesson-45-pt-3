dfac10603a33087c7845f850ec40bb533425f872
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-08-26 16:24:52 -0700

    create account done

--- create-account.js
+++ create-account.js
@@ -1,5 +1,39 @@
 function createAccount(pin, amount) {
-
+    let acctPin = pin
+    let balance = amount || 0;
+    return {
+        checkBalance(pin) {
+            if (pin === acctPin) return `$${balance}`;
+            return "Invalid PIN."
+        },
+        
+        deposit(pin, amount) {
+            if (pin === acctPin) {
+                balance += amount;
+                return `Succesfully deposited $${amount}. Current balance: $${balance}.`
+            }
+            return "Invalid PIN."
+        },
+        
+        withdraw(pin, amount) {
+            if (pin === acctPin) {
+                if (balance > amount) {
+                    balance -= amount;
+                    return `Succesfully withdrew $${amount}. Current balance: $${balance}.`
+                }
+                return "Withdrawal amount exceeds account balance. Transaction cancelled."
+            }
+            return "Invalid PIN."
+        },
+        
+        changePin(oldPin, newPin) {
+            if (oldPin === acctPin) {
+                acctPin = newPin;
+                return "PIN successfully changed!"
+            }
+            return "Invalid PIN."
+        },
+    }
 }
 
 module.exports = { createAccount };
